0aaa62f3bc1e7f33cf3def98716e1bb9fba6f910
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-07-26 11:29:05 +0530

    data types

---
+++ dataTypes/first.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+int main(){
+ 
+    //! int type variable
+    int mark = 99;
+    cout << " mark = " << mark << endl;
+    //! float type variable
+    float pi = 3.14;
+    cout << " pi = " << pi << endl;
+    //! char type variable
+    char grade = 'A';
+    cout << " grade = " << grade << endl;
+    //! string type variable
+    string name = "Monkey D Luffy";
+    cout << " name = " << name << endl;
+    //! boolean type variable
+    bool isCaptain = true;
+    cout << " isCaptain = " << isCaptain << endl;
+    //! double type variable
+    double e = 2.71828;
+    cout << " e = " << e << endl;
+    //! long type variable
+    long population = 7800000000;
+    cout << " population = " << population << endl;
+    //! short type variable
+    short age = 19;
+    cout << " age = " << age << endl;
+    //! unsigned int type variable
+    unsigned int score = 100;
+    cout << " score = " << score << endl;
+    //! long long type variable
+    long long distance = 1234567890123;
+    cout << " distance = " << distance << endl;
+    //! unsigned long long type variable
+    unsigned long long starsInGalaxy = 1000000000000;
+    cout << " starsInGalaxy = " << starsInGalaxy << endl;
+    //! unsigned short type variable
+    unsigned short year = 2023;
+    cout << " year = " << year << endl;
+    
+
+
+    return 0;
+}
