1a6c5299d6b870a8e15eddbad0b2359512ff40ec
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-08-24 11:17:24 +0530

    find missing no with binary serch

---
+++ SEARCHING-SORTING-L1/findMissingElem.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int findMissing(int arr[], int size){
+ int s = 0 ;
+ int e = size -1 ;
+ int m  = s + (e - s ) /2 ;
+ int ans = -1;
+while(s<=e ){
+    int dif =arr[m] - m;
+    if (dif==1)
+    {
+       s = m + 1;
+    }
+    else{
+        ans = m ;
+        e = m -1 ;
+
+    }
+
+
+    m  = s + (e - s ) /2 ;
+
+}
+
+return ans + 1 ;
+
+}
+
+int main(){
+
+  int arr [] = {1,3,4,5,6,7,8,9,10};
+  int size = 8 ;
+  
+int ans =  findMissing(arr,size);
+if(ans){
+    cout << " the missing no is :" << ans << endl;
+
+}
+else{
+    cout << "all elem is avalible"<< endl;
+}
+
+
+    return 0 ;
+}
