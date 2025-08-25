9c6889182eced77f6a80dbd754fa83f8bdd4c8f2
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-08-25 11:00:56 +0530

    pivot elem

---
+++ SEARCHING-SORTING-L2/pivotElem.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int findPivot(int arr[], int n) {
+    int s = 0, e = n - 1;
+    int mid;
+
+    while (s < e) {
+        mid = s + (e - s) / 2;
+
+        // agar mid element last element se bada hai
+        // to pivot right side me hoga
+        if (arr[mid] > arr[e]) {
+            s = mid + 1;
+        }
+        else {
+            // warna pivot left side me ya khud mid hi hai
+            e = mid;
+        }
+    }
+    return arr[s];  // s ya e dono pivot index pe aa jayenge
+}
+
+int main() {
+    int arr[] = {7, 9, 1, 2, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Pivot element is: " << findPivot(arr, n) << endl;
+    return 0;
+}
