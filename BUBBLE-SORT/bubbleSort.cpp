70a0598aaca6717b98f4fd183998bdbf83190f60
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-01 15:56:53 +0530

    bubble sort

---
+++ BUBBLE-SORT/bubbleSort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        // Har pass me ek bada element end me chalega
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {5, 3, 2, 1, 6, 8, 2};
+    int size = 7;
+
+    bubbleSort(arr, size);
+
+    cout << "Sorted array: " << endl;
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
