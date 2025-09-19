4d9d30db07d303e1fdf7d26fdd6a608096795544
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-19 09:13:04 +0530

    IMP

--- CHAR-ARRAY-LEVEL-1/Starting.cpp
+++ CHAR-ARRAY-LEVEL-1/Starting.cpp
@@ -8,7 +8,7 @@ int main(){
 
     //! char array me jab bhi hum input le rhe hoge to isme space ke sace me hum by default ruk jate hai 
     //? like 10 size ke array me maine DING DONG enter kiya to sirf DING print hoga kyuki DING ke bd space hai
-
+    //* bich me ENTER daba diya ya phir TAB daba diya ya phir SPACE daba diya to wo age ke input ko concidor nahi karega 
     cout << "the value of ch is " << ch << endl;
 
 
