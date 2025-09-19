d09493cd0292fae27ef73f3e323cac2e4ebf1ae6
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-19 10:03:54 +0530

    one of the most important

---
+++ CHAR-ARRAY-LEVEL-1/convert.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    //! formula for converting lower case to upper case 
+    //? lowercase( koi sa bhi jisko bade me convert krna hai )
+    //! example c to C
+    //? lowercase  99 - 'a' + 'A'
+    //! 99 - 97 + 65  = 67 
+    //? 67 == C 
+
+
+    //* iska ulta for 'A' to 'a' krna hoga to formula hoga 
+
+
+    //? Upper Char  - 'A' + 'a' 
+    //! D ke liye example dekhte hai 
+    //? 68 - 65 + 97 = 100
+    //! 100 = 'd' 
+
+    
+
+
+    return 0;
+}
