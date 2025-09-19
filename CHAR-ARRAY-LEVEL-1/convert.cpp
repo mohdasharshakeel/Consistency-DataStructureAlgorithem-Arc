325972a1ba8e699dc234eaaf655b76e819691a48
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-19 10:14:29 +0530

    convert to upper

--- CHAR-ARRAY-LEVEL-1/convert.cpp
+++ CHAR-ARRAY-LEVEL-1/convert.cpp
@@ -1,26 +1,40 @@
 #include<iostream>
 using namespace std;
+void convertToUpperCae(char ch[], int size){
+    int index = 0;
+
+    while (ch[index] != '\0')
+    {
+        char currentIndex = ch[index];
+        if(currentIndex >= 'a' && currentIndex <= 'z'){
+            ch[index] = currentIndex - 'a' + 'A';
+        }
+        index++;
+    }
+    
+}
 
 int main(){
 
-    //! formula for converting lower case to upper case 
+    char ch[100];
+    cin >> ch;
+    convertToUpperCae(ch, 100);
+
+    cout << "  to upper case " << ch << endl;
+    
+
+    //! formula for converting lower case to upper case
     //? lowercase( koi sa bhi jisko bade me convert krna hai )
     //! example c to C
     //? lowercase  99 - 'a' + 'A'
-    //! 99 - 97 + 65  = 67 
-    //? 67 == C 
+    //! 99 - 97 + 65  = 67
+    //? 67 == C
 
+    //* iska ulta for 'A' to 'a' krna hoga to formula hoga
 
-    //* iska ulta for 'A' to 'a' krna hoga to formula hoga 
-
-
-    //? Upper Char  - 'A' + 'a' 
-    //! D ke liye example dekhte hai 
+    //? Upper Char  - 'A' + 'a'
+    //! D ke liye example dekhte hai
     //? 68 - 65 + 97 = 100
-    //! 100 = 'd' 
-
-    
-
-
+    //! 100 = 'd'
     return 0;
 }
