041d63b5699b0e430361f6ab3eb056b4cbfd6f4d
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-19 09:17:42 +0530

    cin.getline

--- CHAR-ARRAY-LEVEL-1/Starting.cpp
+++ CHAR-ARRAY-LEVEL-1/Starting.cpp
@@ -4,14 +4,21 @@ using namespace std;
 int main(){
 
     char ch[10];
-    cin >> ch;
+    // cin >> ch;
 
     //! char array me jab bhi hum input le rhe hoge to isme space ke sace me hum by default ruk jate hai 
     //? like 10 size ke array me maine DING DONG enter kiya to sirf DING print hoga kyuki DING ke bd space hai
     //* bich me ENTER daba diya ya phir TAB daba diya ya phir SPACE daba diya to wo age ke input ko concidor nahi karega 
-    //* ye teeno chize cin>> wale case me kam krti hai 
+    //* ye teeno chize cin>> wale case me kam krti hai
 
+    cin.getline(ch, 100);
     
+    /*
+    !orewa ashar 
+    ! the value of ch is orewa ashar 
+    ! cin.getline se agar input lege to ye dikkat avoid kr skte hai 
+    */
+
     cout << "the value of ch is " << ch << endl;
 
 
