f23b1e1f92c528d91867eb0d68500375d8e26196
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-19 09:10:18 +0530

    imp messge for Char array

--- CHAR-ARRAY-LEVEL-1/Starting.cpp
+++ CHAR-ARRAY-LEVEL-1/Starting.cpp
@@ -6,9 +6,12 @@ int main(){
     char ch[10];
     cin >> ch;
 
+    //! char array me jab bhi hum input le rhe hoge to isme space ke sace me hum by default ruk jate hai 
+    //? like 10 size ke array me maine DING DONG enter kiya to sirf DING print hoga kyuki DING ke bd space hai
+
     cout << "the value of ch is " << ch << endl;
 
-    
+
     return 0;
 } 
 
