2a8cc9a38e8d1b87c9ee519755a373cb3a0284fe
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-20 10:02:02 +0530

    string array

---
+++ CHAR-ARRAY-LEVEL-1/StringArray.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    //! why we need string array 
+    //? jaise vector apne size ko change kr skta hai aise hi string apne size ko run time per change kr skti hai dynamicly ghata ya bhada sakti hai 
+    //! creation
+    string st;
+    cin >> st;
+    cout << st <<  endl;
+    cout << st[2] <<  endl;
+
+
+    return 0;
+}
