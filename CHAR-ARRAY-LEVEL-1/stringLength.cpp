f69bc313b4f3c28cf5cd1d5f8636d1a76d47056e
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-20 10:15:17 +0530

    length of string

---
+++ CHAR-ARRAY-LEVEL-1/stringLength.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string name;
+    cin >> name;
+
+
+    cout << " the length of the string is : " << name.length() << endl;
+
+    return 0;
+}
