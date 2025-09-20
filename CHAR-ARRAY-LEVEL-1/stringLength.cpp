497ecab3f05cd882b701e4be606946447aff7380
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-20 10:16:32 +0530

    empty

--- CHAR-ARRAY-LEVEL-1/stringLength.cpp
+++ CHAR-ARRAY-LEVEL-1/stringLength.cpp
@@ -4,8 +4,11 @@ int main(){
     string name;
     cin >> name;
 
-
+   //! length of string
     cout << " the length of the string is : " << name.length() << endl;
+
+    //! is empty 
+    cout << " the sting is empty or not : " << name.empty() << endl;
 
     return 0;
 }
